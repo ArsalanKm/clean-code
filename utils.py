e0a0637f507edf9d7772b7ad722ce38b4da3f3f3
arsalan karimzad <karimzadarsalan@gmail.com>
2022-08-31 17:49:19 +0430

    utils fixes

--- utils.py
+++ utils.py
@@ -35,9 +35,57 @@ class jsonic(object):
     
     def __init__(self, *decorargs, **deckeywords):
         self.deckeywords = deckeywords
+
+    
     
     def __call__(self, fn):
-        def jsoner(obj, **kwargs):
+        def check_incl(incl):
+            if type(incl) == type([]):
+                    thefields.extend(incl)
+            else:
+                thefields.append(incl)
+
+        def check_sk():
+            if type(sk) == type([]):
+                    for skipper in sk:
+                        if skipper in thefields:
+                            thefields.remove(skipper)
+            else:
+                    if sk in thefields:
+                        thefields.remove(sk)
+        
+        def check_key():
+            if hasattr(thedic, "__class__") and hasattr(thedic, "all"):
+                        if callable(thedic.all):
+                            if hasattr(thedic.all(), "json"):
+                                if recurse < recurse_limit:
+                                    kwargs['recurse'] = recurse + 1
+                                    dic[key] = thedic.all().json(**kwargs)
+            elif hasattr(thedic, "json"):
+                        if recurse < recurse_limit:
+                            kwargs['recurse'] = recurse + 1
+                            dic[key] = thedic.json(**kwargs)
+            else:
+                        try:
+                            theuni = thedic.__str__()
+                        except UnicodeEncodeError:
+                            theuni = thedic.encode('utf-8')
+                        dic[key] = theuni
+
+        def iterate():
+             ## now, do we have imagekit stuff in there?
+            if hasattr(obj, "_ik"):
+                if hasattr(obj, obj._ik.image_field):
+                    if hasattr(getattr(obj, obj._ik.image_field), 'size'):
+                        if getattr(obj, obj._ik.image_field):
+                            for ikaccessor in [getattr(obj, s.access_as) for s in obj._ik.specs]:
+                                key = ikaccessor.spec.access_as
+                                dic[key] = {
+                                    'url': ikaccessor.url,
+                                    'width': ikaccessor.width,
+                                    'height': ikaccessor.height,
+                                }
+        def init():
             dic = {}
             key = None
             thedic = None
@@ -48,19 +96,13 @@ class jsonic(object):
             recurse = kwargs.get('recurse', 0)
             incl = kwargs.get('include')
             sk = kwargs.get('skip')
+            
+        def jsoner(obj, **kwargs):
+           
             if incl:
-                if type(incl) == type([]):
-                    thefields.extend(incl)
-                else:
-                    thefields.append(incl)
+                check_incl(incl)
             if sk:
-                if type(sk) == type([]):
-                    for skipper in sk:
-                        if skipper in thefields:
-                            thefields.remove(skipper)
-                else:
-                    if sk in thefields:
-                        thefields.remove(sk)
+                check_sk()
             
             ## first vanilla fields
             for f in thefields:
@@ -78,34 +120,9 @@ class jsonic(object):
                     key = "%s_set" % f
                 
                 if key:
-                    if hasattr(thedic, "__class__") and hasattr(thedic, "all"):
-                        if callable(thedic.all):
-                            if hasattr(thedic.all(), "json"):
-                                if recurse < recurse_limit:
-                                    kwargs['recurse'] = recurse + 1
-                                    dic[key] = thedic.all().json(**kwargs)
-                    elif hasattr(thedic, "json"):
-                        if recurse < recurse_limit:
-                            kwargs['recurse'] = recurse + 1
-                            dic[key] = thedic.json(**kwargs)
-                    else:
-                        try:
-                            theuni = thedic.__str__()
-                        except UnicodeEncodeError:
-                            theuni = thedic.encode('utf-8')
-                        dic[key] = theuni
-            
-            ## now, do we have imagekit stuff in there?
-            if hasattr(obj, "_ik"):
-                if hasattr(obj, obj._ik.image_field):
-                    if hasattr(getattr(obj, obj._ik.image_field), 'size'):
-                        if getattr(obj, obj._ik.image_field):
-                            for ikaccessor in [getattr(obj, s.access_as) for s in obj._ik.specs]:
-                                key = ikaccessor.spec.access_as
-                                dic[key] = {
-                                    'url': ikaccessor.url,
-                                    'width': ikaccessor.width,
-                                    'height': ikaccessor.height,
-                                }
+                    check_key()   
+
+            iterate() 
+
             return fn(obj, json=dic, **kwargs)
         return jsoner
